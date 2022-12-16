7801b40547159bb0ee5c166c334043bb78d538c1
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-12-16 17:35:52 +0100

    added footer partial

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,82 @@
 <footer>
-    Footer
+    <!-- footer top -->
+    <div class="footer_top">
+        <div class="container">
+            <div class="row row-cols-2">
+                <div class="col py-4">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col px-0">
+                                <h5 class="text-uppercase">DC Comics</h5>
+                                <ul>
+                                    <li><a href="/characters">Characters</a></li>
+                                    <li><a href="/">Comics</a></li>
+                                    <li><a href="/movies">Movies</a></li>
+                                    <li><a href="/tv">TV</a></li>
+                                    <li><a href="/games">Games</a></li>
+                                    <li><a href="/videos">Videos</a></li>
+                                    <li><a href="/news">News</a></li>
+                                </ul>
+
+                                <h5 class="text-uppercase">Shop</h5>
+                                <ul>
+                                    <li><a href="/">Shop DC</a></li>
+                                    <li><a href="/collectibles">Shop DC Collectibles</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col px-0">
+                                <h5 class="text-uppercase">DC</h5>
+                                <ul>
+                                    <li><a href="/">Terms Of Use</a></li>
+                                    <li><a href="/">Privacy policy (New)</a></li>
+                                    <li><a href="/">Ad Choices</a></li>
+                                    <li><a href="/">Advertising</a></li>
+                                    <li><a href="/">Jobs</a></li>
+                                    <li><a href="/">Subscriptions</a></li>
+                                    <li><a href="/">Talent Workshops</a></li>
+                                    <li><a href="/">CPSC Certificates</a></li>
+                                    <li><a href="/">Ratings</a></li>
+                                    <li><a href="/">Shop Help</a></li>
+                                    <li><a href="/">Contact Us</a></li>
+                                </ul>
+                            </div>
+
+                            <div class="col px-0">
+                                <h5 class="text-uppercase">Sites</h5>
+                                <ul>
+                                    <li><a href="/">DC</a></li>
+                                    <li><a href="/">MAD Magazine</a></li>
+                                    <li><a href="/">DC Kids</a></li>
+                                    <li><a href="/">DC Universe</a></li>
+                                    <li><a href="/">DC Power Visa</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    All Site Content TM and &copy; 2020 DC Entertainment unless otherwise <span>noted here</span>. All rights reserved.
+                    <span>Cookies Settings</span>
+                </div>
+
+                <div class="col dc_logo_bg"></div>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer bottom -->
+    <div class="footer_bottom py-4">
+        <div class="container d-flex justify-content-between align-items-center">
+            <button class="p-2 text-uppercase">Sign-Up Now!</button>
+
+            <div class="d-flex align-items-center gap-3 text-uppercase">
+                Follow Us
+                <img src="{{ Vite::asset('resources/img/footer-facebook.png') }}" alt="facebook-logo">
+                <img src="{{ Vite::asset('resources/img/footer-twitter.png') }}" alt="twitter-logo">
+                <img src="{{ Vite::asset('resources/img/footer-youtube.png') }}" alt="youtube-logo">
+                <img src="{{ Vite::asset('resources/img/footer-pinterest.png') }}" alt="pinterest-logo">
+                <img src="{{ Vite::asset('resources/img/footer-periscope.png') }}" alt="periscope-logo">
+            </div>
+        </div>
+    </div>
 </footer>
