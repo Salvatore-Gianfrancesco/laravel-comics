aebc88ee46dda23c88b9e6eb106205e7829b2281
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-12-16 15:52:02 +0100

    added header partial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>DC @yield('page-title', 'Comics')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- FontAwesome CDN -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    <!-- header -->
+    @include('partials.header')
+    <!-- /header -->
+
+    <main>
+        @yield('content')
+    </main>
+
+    <!-- footer -->
+    @include('partials.footer')
+    <!-- /footer -->
+</body>
+
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,36 @@
+<header>
+    <div class="header_top">
+        <div class="container d-flex justify-content-end align-items-center gap-5 py-1 text-uppercase">
+            <a href="/">DC Power&trade;Visa&reg;</a>
+            <a href="/">Additional DC Sites</a>
+        </div>
+    </div>
+
+    <div class="header_bottom py-3">
+        <div class="container d-flex justify-content-between">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo">
+
+            <nav class="d-flex align-items-center gap-4 text-uppercase">
+                <a href="/" class="{{Route::currentRouteName() === 'characters' ? 'active' : ''}}">characters</a>
+                <a href="/" class="{{Route::currentRouteName() === 'home' ? 'active' : ''}}">comics</a>
+                <a href="/" class="{{Route::currentRouteName() === 'movies' ? 'active' : ''}}">movies</a>
+                <a href="/" class="{{Route::currentRouteName() === 'tv' ? 'active' : ''}}">tv</a>
+                <a href="/" class="{{Route::currentRouteName() === 'games' ? 'active' : ''}}">games</a>
+                <a href="/" class="{{Route::currentRouteName() === 'collectibles' ? 'active' : ''}}">collectibles</a>
+                <a href="/" class="{{Route::currentRouteName() === 'videos' ? 'active' : ''}}">videos</a>
+                <a href="/" class="{{Route::currentRouteName() === 'fans' ? 'active' : ''}}">fans</a>
+                <a href="/" class="{{Route::currentRouteName() === 'news' ? 'active' : ''}}">news</a>
+                <a href="/" class="{{Route::currentRouteName() === 'shop' ? 'active' : ''}}">shop
+                    <i class="fa-solid fa-caret-down"></i>
+                </a>
+            </nav>
+
+            <div class="search d-flex align-items-center">
+                <div class="text-end gap-2">
+                    Search
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
