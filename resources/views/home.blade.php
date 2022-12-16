d674d16c03cfebb101eac427a1f14e838b0c24da
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-12-16 16:45:26 +0100

    added homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,66 @@
 @extends('layouts.app')
+
+@section('content')
+<!-- jumbotron -->
+<div class="jumbotron">
+    <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}} " alt="jumbotron">
+</div>
+
+<!-- current series -->
+<div class="current_series pt-4 pb-3">
+    <div class="container">
+        <div class="row row-cols-6 g-3">
+            @foreach($comics as $comic)
+            <div class="col">
+                <div class="card border-0 rounded-0 text-uppercase">
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}" class="my-2">
+                    {{ $comic['series'] }}
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+
+<!-- buy comics -->
+<div class="buy_comics py-5">
+    <div class="container">
+        <div class="row row-cols-5">
+            <div class="col">
+                <div class="text-uppercase">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="">
+                    Digital Comics
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="text-uppercase">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="">
+                    DC Merchandise
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="text-uppercase">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="">
+                    Subscription
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="text-uppercase">
+                    <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="">
+                    Comic Shop Locator
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="text-uppercase">
+                    <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="">
+                    DC Power Visa
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,9 +6,9 @@
         </div>
     </div>
 
-    <div class="header_bottom py-3">
+    <div class="header_bottom my-1">
         <div class="container d-flex justify-content-between">
-            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="dc-logo" class="my-3">
 
             <nav class="d-flex align-items-center gap-4 text-uppercase">
                 <a href="/" class="{{Route::currentRouteName() === 'characters' ? 'active' : ''}}">characters</a>
@@ -20,13 +20,13 @@
                 <a href="/" class="{{Route::currentRouteName() === 'videos' ? 'active' : ''}}">videos</a>
                 <a href="/" class="{{Route::currentRouteName() === 'fans' ? 'active' : ''}}">fans</a>
                 <a href="/" class="{{Route::currentRouteName() === 'news' ? 'active' : ''}}">news</a>
-                <a href="/" class="{{Route::currentRouteName() === 'shop' ? 'active' : ''}}">shop
+                <a href="/" class="gap-1{{Route::currentRouteName() === 'shop' ? 'active' : ''}}">shop
                     <i class="fa-solid fa-caret-down"></i>
                 </a>
             </nav>
 
             <div class="search d-flex align-items-center">
-                <div class="text-end gap-2">
+                <div class="text-end">
                     Search
                     <i class="fa-solid fa-magnifying-glass"></i>
                 </div>
